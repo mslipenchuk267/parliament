3d8a379ad804cb2931be5711400a7351c509e3c5
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-09-30 14:32:54 -0400

    Setup ble scanner for iOS

--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -1,16 +1,50 @@
 import React from 'react'
-import { Text, View, StyleSheet, SafeAreaView } from 'react-native'
+import { Text, View, Button, StyleSheet, SafeAreaView } from 'react-native'
+import { BleManager } from 'react-native-ble-plx'
+
+const bleManager = new BleManager();
 
 const HomeScreen = () => {
+
+    const handleStartContactTracing = async () => {
+        bleManager.startDeviceScan(
+            null,
+            { allowDuplicates: true },
+            async (error, device) => {
+                console.log("Scanned a device with id: " + device.id)
+            }
+          )
+    }
+
+    const handleStopContactTracing = () => {
+        bleManager.stopDeviceScan();
+    }
+
     return (
-        <SafeAreaView>
-            <Text>This is the home screen</Text>
+        <SafeAreaView style={styles.container} >
+            <View>
+                <Button
+                    title='Start Contact Tracing'
+                    onPress={handleStartContactTracing}
+                />
+            </View>
+            <View>
+                <Button
+                    title='Stop Contact Tracing'
+                    onPress={handleStopContactTracing}
+                />
+            </View>
         </SafeAreaView>
     )
 };
 
 const styles = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
 
 export default HomeScreen;
