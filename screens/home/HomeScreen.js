5a0766aee2d057a7b0e5c6a90355a70ff8269d7e
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-09-30 16:04:53 -0400

    Setup advertising on Android. Succesfully advertising parliament contact tracing service, and narrowed scanning for this service

--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { Text, Platform, PermissionsAndroid, View, Button, StyleSheet, SafeAreaView } from 'react-native'
 import { BleManager } from 'react-native-ble-plx'
+import BLEPeripheral from 'react-native-ble-peripheral';
 
 const bleManager = new BleManager();
 
@@ -8,16 +9,37 @@ const HomeScreen = () => {
 
     const handleStartContactTracing = async () => {
         bleManager.startDeviceScan(
-            null,
+            ['00001200-0000-1000-8000-00805f9b34fb'],
             { allowDuplicates: true },
             async (error, device) => {
-                console.log("Scanned a device with id: " + device.id)
+                console.log("Scanned a device with name: " + device.name + " | " + device.rssi)
             }
-          )
+        )
     }
 
     const handleStopContactTracing = () => {
         bleManager.stopDeviceScan();
+    }
+
+    const handleStartAdvertising = () => {
+        if (Platform.OS === 'android') {
+            BLEPeripheral.setName('Parliament Android');
+            BLEPeripheral.addService('00001200-0000-1000-8000-00805f9b34fb', true);
+            //BLEPeripheral.addCharacteristicToService('f4c47b4e-18e2-4697-bc39-01de39b9bae5', '76c11234-9b41-4e13-a229-e29059c00e47', 16|1, 8 )
+
+            BLEPeripheral.start()
+                .then(res => {
+                    console.log(res)
+                }).catch(error => {
+                    console.log(error)
+                })
+        }
+    }
+
+    const handleStopAdvertising = () => {
+        if (Platform.OS === 'android') {
+            BLEPeripheral.stop()
+        }
     }
 
     useEffect(() => {
@@ -48,6 +70,18 @@ const HomeScreen = () => {
                 <Button
                     title='Stop Contact Tracing'
                     onPress={handleStopContactTracing}
+                />
+            </View>
+            <View>
+                <Button
+                    title='Start Advertising'
+                    onPress={handleStartAdvertising}
+                />
+            </View>
+            <View>
+                <Button
+                    title='Stop Advertising'
+                    onPress={handleStopAdvertising}
                 />
             </View>
         </SafeAreaView>
