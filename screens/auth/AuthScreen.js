41ac6a383a5d6b0c266161efe2b23d7c8e6187b8
chenerzhang <tuh12085@temple.edu>
2020-10-12 13:44:56 -0400

    Add Login and Logout Button

--- screens/auth/AuthScreen.js
+++ screens/auth/AuthScreen.js
@@ -6,8 +6,17 @@ const AuthScreen = () => {
     return (<View>
         <Text>AuthScreen  </Text>
 
+        <Button title="Login" onPress={() => {
+            console.log("You click on the Login Button");
+        }} />
+
+        <Button title="Logout" onPress={() => {
+            console.log("You click on the Logout Button");
+        }} />
     </View>
     );
 
 }
+
+const styles = StyleSheet.create({});
 export default AuthScreen;
