d4bd922152e839eb81a31740edeb736f76ee7e8f
chenerzhang <tuh12085@temple.edu>
2020-10-12 13:51:50 -0400

    Add Sign in and Sign up Screen
    - Screens haven't connected yet

---
+++ screens/auth/Sign_In_Screen.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+
+const styles = StyleSheet.create({});
+export default Sign_In_Screen;
---
+++ screens/auth/Sign_up_Screen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+
+
+
+const Sign_up_Screen = () => {
+    return (<View>
+        <Text>Sign_up_Screen  </Text>
+
+
+    </View>
+    );
+
+}
+const styles = StyleSheet.create({});
+export default Sign_up_Screen;
