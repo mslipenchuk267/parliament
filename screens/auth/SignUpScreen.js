5d7845390832c91cd02ea0d38a21b0e28e39838d
Nephoros <tuk39399@temple.edu>
2020-10-15 16:00:54 -0400

    Added signUpButtonHandler and linked it to attemptSignup action

--- screens/auth/SignUpScreen.js
+++ screens/auth/SignUpScreen.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import { View, StyleSheet, Button,Text,TextInput } from 'react-native';
+import React,{useState} from 'react';
+import {useDispatch} from 'react-redux';
+import { View, StyleSheet, Button,Text,TextInput} from 'react-native';
 import CustomTextInput from '../../Components/CustomTextInput';
+import * as userActions from '../../store/actions/user';
+
+
 
 
 
 const Sign_up_Screen = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+    const signUpButtonHandler = () => {
+        dispatch(userActions.attemptSignup(username,password))
+    }
+
     return (<View>
-        <CustomTextInput placeholder="Enter Username"/>
-        <CustomTextInput placeholder="Enter Password" secureTextEntry={true}/>
-        <Button title="Sign Up"/>
+        <CustomTextInput
+        placeholder="Enter Username"
+        onChangeText={(text) => setUsername(text)}/>
+        <CustomTextInput 
+        placeholder="Enter Password"
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry={true}/>
+        <Button title="Sign Up" onPress={signUpButtonHandler}/>
     </View>
     );
 
