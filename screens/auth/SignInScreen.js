07b7bd22faa07beb8f3f773a76ee6b989762625b
Zach Essel <tuk01401@temple.edu>
2020-10-16 10:56:42 -0400

    Refactor buttons to pressables.  Add nested Views

--- screens/auth/SignInScreen.js
+++ screens/auth/SignInScreen.js
@@ -1,7 +1,7 @@
 import { CommonActions } from '@react-navigation/native';
 import React,{useState} from 'react';
 import {useDispatch} from 'react-redux';
-import { View, StyleSheet, Button,Text,TextInput} from 'react-native';
+import { ImageBackground, Pressable, View, StyleSheet, Button,Text,TextInput} from 'react-native';
 import CustomTextInput from '../../Components/CustomTextInput';
 import * as userActions from '../../store/actions/user';
 
@@ -16,18 +16,69 @@ const SignInScreen = () => {
     }
 
 
-    return (<View>
-        <CustomTextInput
-        placeholder="Enter Username"
-        onChangeText={(text) => setUsername(text)}/>
-        <CustomTextInput 
-        placeholder="Enter Password"
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry={true}/>
-        <Button title="Sign In" onPress={signInButtonHandler}/>
+    return (<View >
+        <ImageBackground source={require('../../images/vectorowl.png')}  style={{width: '100%', height: '100%'}}>
+            <View style={{padding:70}}></View>
+            <View style={styles.textInputShape}>
+                <CustomTextInput 
+                placeholder="Enter Username"
+                onChangeText={(text) => setUsername(text)}/>
+            </View>
+            <View style={{padding:10}}></View>
+            <View style={styles.textInputShape}>
+                <CustomTextInput 
+                placeholder="Enter Password"
+                onChangeText={(text) => setPassword(text)}
+                secureTextEntry={true}/>
+            </View>
+            <View style={{padding:10}}></View>
+            <View style={styles.containSizing}>
+                <Pressable
+                    onPress={signInButtonHandler}
+                    style={({ pressed }) => [
+                    {
+                        backgroundColor: pressed
+                        ? "#479cfe" 
+                        : "#fe479c"
+                    },
+                    styles.buttonShape
+                    ]}>
+                    <Text style={styles.buttonText}>
+                        Sign in
+                    </Text>
+                </Pressable>
+            </View>
+        </ImageBackground>
     </View>
     );
 
 }
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textInputShape: {
+        elevation: 10,
+        paddingVertical: 2,
+        paddingHorizontal: 5,
+        backgroundColor: "#497bfe",
+
+      },
+    containSizing: {
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10
+      },
+
+    buttonShape: {
+        elevation: 10,
+        borderRadius: 15,
+        paddingVertical: 20,
+        paddingHorizontal: 50
+      },
+    buttonText: {
+        fontSize: 18,
+        color: "#000",
+        fontWeight: "bold",
+        alignSelf: "center",
+        textTransform: "uppercase"
+      }
+});
 export default SignInScreen;
--- screens/auth/SignUpScreen.js
+++ screens/auth/SignUpScreen.js
@@ -1,17 +1,87 @@
-import React from 'react';
-import { View, StyleSheet, Button,Text,TextInput } from 'react-native';
+import React,{useState} from 'react';
+import {useDispatch} from 'react-redux';
+import { ImageBackground, Pressable, View, StyleSheet, Button,Text,TextInput} from 'react-native';
 import CustomTextInput from '../../Components/CustomTextInput';
+import * as userActions from '../../store/actions/user';
+
+
 
 
 
 const Sign_up_Screen = () => {
-    return (<View>
-        <CustomTextInput placeholder="Enter Username"/>
-        <CustomTextInput placeholder="Enter Password" secureTextEntry={true}/>
-        <Button title="Sign Up"/>
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+    const signUpButtonHandler = () => {
+        dispatch(userActions.attemptSignup(username,password))
+    }
+
+    return (<View >
+        <ImageBackground source={require('../../images/vectorowl.png')}  style={{flex: 1, resizeMode: "cover", justifyContent: "center"}}>
+            <View style={{padding:70}}></View>
+            <View style={styles.textInputShape}>
+                <CustomTextInput 
+                placeholder="Enter Username"
+                onChangeText={(text) => setUsername(text)}/>
+            </View>
+            <View style={{padding:10}}></View>
+            <View style={styles.textInputShape}>
+                <CustomTextInput 
+                placeholder="Enter Password"
+                onChangeText={(text) => setPassword(text)}
+                secureTextEntry={true}/>
+            </View>
+            <View style={{padding:10}}></View>
+            <View style={styles.containSizing}>
+                <Pressable
+                    onPress={signUpButtonHandler}
+                    style={({ pressed }) => [
+                    {
+                        backgroundColor: pressed
+                        ? "#479cfe" 
+                        : "#a947fe"
+                    },
+                    styles.buttonShape
+                    ]}>
+                    <Text style={styles.buttonText}>
+                        Sign up
+                    </Text>
+                </Pressable>
+            </View>
+        </ImageBackground>
     </View>
     );
 
 }
+
+const styles = StyleSheet.create({
+    textInputShape: {
+        elevation: 10,
+        paddingVertical: 2,
+        paddingHorizontal: 5,
+        backgroundColor: "#497bfe",
+
+      },
+    containSizing: {
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10
+      },
+
+    buttonShape: {
+        elevation: 10,
+        borderRadius: 15,
+        paddingVertical: 20,
+        paddingHorizontal: 50
+      },
+    buttonText: {
+        fontSize: 18,
+        color: "#000",
+        fontWeight: "bold",
+        alignSelf: "center",
+        textTransform: "uppercase"
+      }
+});
 
 export default Sign_up_Screen; 
