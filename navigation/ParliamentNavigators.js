bab913dfbe81fad90e8efb95180a5d5df85f3464
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-10-18 21:11:54 -0400

    Improved formatting of headers in auth navigator

--- navigation/ParliamentNavigators.js
+++ navigation/ParliamentNavigators.js
@@ -2,56 +2,59 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+// UserNavigator screens 
 import HomeScreen from '../screens/home/HomeScreen';
 import DiagnosisScreen from '../screens/diagnosis/DiagnosisScreen';
 import NotificationsScreen from '../screens/notifications/NotificationsScreen';
 import SettingsScreen from '../screens/settings/SettingsScreen';
-
-
+// AuthNavigator screens
 import AuthScreen from '../screens/auth/AuthScreen';
 import SignInScreen from '../screens/auth/SignInScreen';
 import SignUpScreen from '../screens/auth/SignUpScreen';
 
 
-// Bottom Tab Navigator
+// UserNavigator Bottom Tab Component
 const UserTabNavigator = createBottomTabNavigator();
-
-// Stack Navigators
+// UserNavigator Stack Navigators (tabs)
 const HomeStackNavigator = createStackNavigator();
 const DiagnosisStackNavigator = createStackNavigator();
 const NotificationStackNavigator = createStackNavigator();
 const SettingsStackNavigator = createStackNavigator();
 
+// AuthNavigator Stack Component 
 const AuthStackNavigator = createStackNavigator();
 
-
-
-
-
-
-//AuthStackNavigator
+/**
+ * The AuthNavigator is a stack navigator
+ * that contains all screens needed for user
+ * authentication. It manages the authentication,
+ * sign in, and sign up screens. 
+ * @example
+ * return (
+ *   <AuthNavigator />
+ * )
+ */
 export const AuthNavigator = () => {
     return (
         <AuthStackNavigator.Navigator>
             <AuthStackNavigator.Screen
                 name="Auth"
                 component={AuthScreen}
+                options={{ headerShown: false }}
             />
             <AuthStackNavigator.Screen
                 name="SignIn"
                 component={SignInScreen}
+                options={{ headerBackTitle: false, headerTitle: "Sign In" }}
             />
             <AuthStackNavigator.Screen
                 name="SignUp"
                 component={SignUpScreen}
+                options={{ headerBackTitle: false, headerTitle: "Sign Up" }}
             />
         </AuthStackNavigator.Navigator>
     )
 }
-
-
-
-
 
 /**
  * The HomeNavigator represents the home tab
