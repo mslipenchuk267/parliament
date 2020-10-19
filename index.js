0727e31b17ccf737600df0946693d4baf4e79da4
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-10-19 04:47:19 -0400

    Added changes that got builds working

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-const appNamePlatform = Platform.OS === 'android' ? "parliament_tu" : "parliament"
+const appNamePlatform = Platform.OS === 'android' ? "parliament_temple" : "parliament"
 
 AppRegistry.registerComponent(appNamePlatform, () => App);
