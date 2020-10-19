414d3e7878c7c7c4901def37eb9dbbfd9693ff67
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-10-19 11:27:49 -0400

    build hot fix

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-const appNamePlatform = Platform.OS === 'android' ? "parliament_temple" : "parliament"
+const appNamePlatform = Platform.OS === 'ios' ? "parliament" : "parliament_temple"
 
 AppRegistry.registerComponent(appNamePlatform, () => App);
