77ec684cd6098313ecb846c971b79140bdf24635
Rathanank Onn <tuk86465@temple.edu>
2020-10-28 16:04:05 -0400

    import and added QR Code library and code to run Camera inside QRScanningComponentScreen, also when scanning QR code, logs data onto console.

--- screens/diagnosis/QRScanningComponentScreen.js
+++ screens/diagnosis/QRScanningComponentScreen.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import { Text, View, StyleSheet, SafeAreaView } from 'react-native'
+import React, { Component } from 'react'
+import { Text, TouchableOpacity, View, StyleSheet, SafeAreaView } from 'react-native'
 
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
 /**
  * The QRScanningComponentScreen component houses the UI components 
  * @example
@@ -9,14 +11,46 @@ import { Text, View, StyleSheet, SafeAreaView } from 'react-native'
  * )
  */
 
-const QRScanningComponentScreen = () => {
+class QRScanningComponentScreen extends Component {
+
+    state ={
+        qr: ""
+    }
+
+    onRead = e => {
+        //this.setState( {qr: e.data} )
+        console.log(e)
+    }
+
+    //Logic to implement QR Scanning
+
+    //Load Camera to Scan QR Code
+
+    //Handle Result of Scan ; possible cases {positive,negative,neither}
+
+    //if result = positive
+        //upload {tempID} -> Infection API
+
+    //else if result = negative
+        //log negative result
+        //Alert user on component to keep staying safe
+     
+    //else(neither)
+        //Alert that QR Code was invalid, try again.
+render() {
     return (
         <SafeAreaView>
-            <Text>Clicked on "Submit QR Code" button to get here</Text>
+
+            <Text>Clicked on "Submit QR Code" button to get here</Text>    
+            
+            <QRCodeScanner
+            onRead={this.onRead}
+            />
+
         </SafeAreaView>
     )
 };
-
+}
 const styles = StyleSheet.create({
     
 });
