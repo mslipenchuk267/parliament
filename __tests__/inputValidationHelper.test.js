051ace0d7af4a570f9ac629df2e45c2bf904668c
chenerzhang <tuh12085@temple.edu>
2020-11-06 11:58:23 -0500

    Add the test

--- __tests__/inputValidationHelper.test.js
+++ __tests__/inputValidationHelper.test.js
@@ -1,6 +1,7 @@
-const { userNameInputValidator } = require("../helpers/inputValidationHelper")
+const { userNameInputValidator, passwordInputValidator } = require("../helpers/inputValidationHelper")
 
 describe('userNameInputValidator', () => {
+    //Username check
     it('should return false if username is less than 4 chars', () => {
         const result = userNameInputValidator("abc");
         const expectedResult = false
@@ -36,5 +37,64 @@ describe('userNameInputValidator', () => {
         expect(result).toEqual(expectedResult)
     })
 
+    //Passwords check
 
+    //True:     
+
+    //Passwords == 10
+    it('should return true if passwords length equals to 10', () => {
+        const result = passwordInputValidator("nhKmK4f4QY").numberValid;
+        const expectedResult = true
+        expect(result).toEqual(expectedResult)
+    })
+
+    //Passwords >=10 && < 24
+    it('should return true if passwords length greater equals to 10 and less than 24', () => {
+        const result = passwordInputValidator("4dztUk7pVG2GNwznptW").lengthValid;
+        const expectedResult = true
+        expect(result).toEqual(expectedResult)
+    })
+
+    //Passwords contains uppercase,lowercase, and numebers
+    it('should return true if Passwords contains uppercase,lowercase, and numebers', () => {
+        const result = passwordInputValidator("fGuED93mw2eh5tbtngX").isValid;
+        const expectedResult = true
+        expect(result).toEqual(expectedResult)
+    })
+
+    //False:
+    //Passwords < 10
+    it('should return false if Passwords length less than 10', () => {
+        const result = passwordInputValidator("DuWp65h").lengthValid;
+        const expectedResult = false
+        expect(result).toEqual(expectedResult)
+    })
+
+    //Passwords > 24
+    it('should return false if Passwords length greater than 24', () => {
+        const result = passwordInputValidator("3WxqC559sgaeckWKqVxQZuS5y").lengthValid;
+        const expectedResult = false
+        expect(result).toEqual(expectedResult)
+    })
+
+    //Passwords do not contains uppercase
+    it('should return false if Passwords do not contains uppercase', () => {
+        const result = passwordInputValidator("t74q7s8srwdq").caseValid;
+        const expectedResult = false
+        expect(result).toEqual(expectedResult)
+    })
+
+    //Passwords do not contains lowercase
+    it('should return false if Passwords do not contains lowercase', () => {
+        const result = passwordInputValidator("WGPH9U6SPEYP").caseValid;
+        const expectedResult = false
+        expect(result).toEqual(expectedResult)
+    })
+
+    //Passwords do not contains numbers
+    it('should return false if Passwords do not contains number', () => {
+        const result = passwordInputValidator("ppNfpqUprYUC").numberValid;
+        const expectedResult = false
+        expect(result).toEqual(expectedResult)
+    })
 })
